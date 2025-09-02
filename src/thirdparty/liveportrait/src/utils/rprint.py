2b0676d2c245a5fbada18b3ac56dfb176a3bb745
Krish Anjara <127621879+krish-coder-24@users.noreply.github.com>
2025-09-03 04:13:47 +0530

    Update rprint.py

--- src/thirdparty/liveportrait/src/utils/rprint.py
+++ src/thirdparty/liveportrait/src/utils/rprint.py
@@ -5,8 +5,11 @@ custom print and log functions
 """
 
 from rich import print
+import time
 
 __all__ = ['rprint', 'rlog']
 
 rprint = print
-rlog = lambda msg: print(f"[cyan]{time.ctime().split()[3]}[/cyan] [green]INFO[/green] - {msg}")
+rlog = lambda msg: print(
+    f"[cyan]{time.ctime().split()[3].replace(':', '[/cyan][white]:[/][cyan]')}[/cyan] [green]INFO[/green] - {msg}"
+)
